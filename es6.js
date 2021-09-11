eef87ec2f71e9e6e606c1494ade965f79b40d081
masum691 <masumhu691@gmail.com>
2021-09-11 17:31:53 +0600

    es6

--- es6.js
+++ es6.js
@@ -3,6 +3,24 @@
 1. template string (` `)
 
 2. arrow function
-   const getFiftyFive = () 
+   const getFiftyFive = () => 55;
+   const getSixtySix = num => num + 66;
+   const isEven = x => x % 2 == 0;
+   const addThree = (x, y, z) => x + y + z;
+   const doMath = (num1, num2) => {
+       const sum = num1 + num2;
+       return sum;
+   }
+
+3. spread operator ( ... ) *** watch this modules video
+   const numbers = [121,23,34,235,56]
+   const newNumbers = [...numbers];
+   numbers.push(65);
+   numbers.push(35);
+   numbers.push(75);
+   console.log(newNumbers)
+create a new array from an older array and add an element
+
+
 
 */
