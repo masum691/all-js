fd4a133163bc49727c6e949bf86da1409c993958
masum691 <masumhu691@gmail.com>
2021-09-11 17:20:27 +0600

    all js concepts ready for react

---
+++ es6.js
@@ -0,0 +1,8 @@
+
+/* 
+1. template string (` `)
+
+2. arrow function
+   const getFiftyFive = () 
+
+*/
---
+++ fundementals.js
@@ -0,0 +1,19 @@
+
+/*
+1. how to declare a variable using let and const
+
+2. how to declare a condition (6 basic condition_  >  ,< , === , !== , <= , >= ) and multiple conditions: && , ||
+
+3. how to declare an array
+   array index, length, push, pop, shift, unshift
+
+4. loop (for , while) also for in , and for of
+
+5. how to declare a function, arrow function
+
+6. declare Object 
+   console.log(student.age);
+   console.log(student['age']);
+   console.log(student[myVariable]);
+
+*/
